fe2d46735a6bb4d52b0d65c1ab5c9835a4024cd5
Þorlákur Lyngmo <thorlakur16@ru.is>
2017-10-02 15:34:39 +0000

    Task 4
    Add will not allow negative numbers.

--- src/TDD/StringCalculator.java
+++ src/TDD/StringCalculator.java
@@ -3,11 +3,15 @@ package TDD;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
     public static int Add(String numbers){
 
         String[] numbersArray = numbers.split(",");
+        ArrayList<Integer> negative = new ArrayList<Integer>();
         int result = 0;
         if(numbers.length() == 0){
             return 0;
@@ -20,14 +24,32 @@ public class StringCalculator {
                 result = result+Integer.parseInt(numbersArray[i]);
             }
         }
-        return result;
+        for(int i = 0; i < numbersArray.length; i++){
+            if(Integer.parseInt(numbersArray[i]) < 0){
+                negative.add(Integer.parseInt(numbersArray[i]));
+            }
+        }
+
+        if(negative.isEmpty()){
+            return result;
+        }
+        else{
+            StdOut.print("Negative numbers not allowed: ");
+            for(int i = 0; i < negative.size(); i++){
+                StdOut.print(negative.get(i));
+                StdOut.print(" ");
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
         String s = StdIn.readLine().toString();
         s = s.replace("\\n","," );
         int total = Add(s);
-        StdOut.print(total);
+        if(total != -1){
+            StdOut.print(total);
+        }
     }
 
 }
