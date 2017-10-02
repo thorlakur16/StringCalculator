76be644a0b068609f7fd426b08f1a2559fc38e6e
Þorlákur Lyngmo <thorlakur16@ru.is>
2017-10-02 15:03:36 +0000

    task 1
    Add for 0,1 og 2 numbers

---
+++ src/TDD/StringCalculator.java
@@ -0,0 +1,32 @@
+package TDD;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class StringCalculator {
+
+    public static int Add(String numbers){
+
+        String[] numbersArray = numbers.split(",");
+        int result = 0;
+        if(numbers.length() == 0){
+            return 0;
+        }
+        else if(numbersArray.length == 1){
+            return Integer.parseInt(numbers);
+        }
+        else if(numbersArray.length == 2){
+            for(int i = 0; i < numbersArray.length; i++){
+                result = result+Integer.parseInt(numbersArray[i]);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = StdIn.readLine().toString();
+        int total = Add(s);
+        StdOut.print(total);
+    }
+
+}
